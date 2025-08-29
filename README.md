e3515c420c99dae42b1a845c01eb8f3b87fb6007
Sparkes7 <willsparkes93@gmail.com>
2025-08-29 19:03:23 +0100

    core game created

---
+++ README.md
@@ -0,0 +1,51 @@
+# Goals of the assignment
+
+✅ ❌
+
+## Basic Requirements:
+
+✅ Fetch upgrade data from the provided API and at least one upgrade from the API update the cookie count.
+✅ Ensure that functions are used effectively to keep code organised and reusable.
+✅ Implement event listeners to handle user interactions.
+✅ Use local storage to save and restore the cookie count and relevant game information.
+✅ Use setInterval to increment the cookie count and manage the game state each second.
+Managing the game state includes saving progress and updating the DOM.
+
+## Stretch Goals:
+
+✅ Consolidate upgrade management by managing all upgrades in a single function.
+❌ Improve UX with animations, sound effects, or other visual effects.
+❌ Fantastic use of README to provide important information such as a description of the project, how to deploy and other app information.
+❌ Implement error handling using try/catch.
+❌ Create a menu for users to adjust game options like sound effects or display preferences.
+
+# Reflection
+
+This weeks assignment was to create a cookie-clicker style game. I had some ideas going into this but due to the specific API data and goal requirements, I decided to pivot to a more traditional cookie clicker game, then add some flair in at the end if I had time. Below I'm going to talk about some specific aspects of the development process that I have thoughts about, or points I wanted to share.
+
+## The Tooltip, and Design Principles!
+
+In the original Cookie Clicker game, there is a feature that when you hover over an upgrade, you get a tooltip with some functional text as to what the upgrade is doing, and also some flavour text to go along with it.
+
+One of the core design principles that I wanted was for the game to feel relaxed and minimalist. I really wanted to produce this tooltip to help with this design principle as it frees the screen up from information when you don't particularly need it.
+
+To achieve the tooltip, I created a tooltip `<div>` for this information to be in. The initial idea was to have the tooltip be in a set place but would be annoying to hover over an upgrade, then have to divert your eyes elsewhere to read the tooltip, then back to the mouse cursor. Like other games i've played, tooltips generally appear at the mouse cursor and so I searched for ways to get my mouse position so that I could position my tooltip with absolute positioning - and I have to say, i'm very pleased with that and it works very well!
+
+## Multiple Fetch Requests
+
+**The Problem**
+When implementing my hover tooltip, I decided that the best method I could take was to dynamically take the data from the API data, that way if the upgrades changed or more were added, the tooltip would be able to update regardless without the need to hard-code in some values.
+I discovered a problem when I got the tooltip to work, in that it was slightly delayed when hovering. BEHOLD: DOZENS OF FETCH REQUESTS.
+
+![Fetch Requests](https://drive.google.com/file/d/1PbdtsGhxRdJBN7yPlOIH-SgE5kfW9LPK/view?usp=drive_link)
+
+**Solution**
+Every time I hovered an upgrade, it was fetching the data from the API. Because the speed of my tooltip updating was bottlenecked by the speed of the fetch, I decided that the best way to handle this would be to initially fetch the API data, then loop through the API array and push the values into my own local array, then use that new local array as my reference point.
+
+This means that the fetch request only happens once on page load, and if changes are made to the API, all it would require is a page refresh -> the API would have new data -> new data is reflected in my local array (hopefully!).
+
+# Resources:
+
+[Get Mouse Position in Javascript](https://medium.com/@ryan_forrester_/how-to-get-mouse-position-in-javascript-37e4772a3f21)
+
+[Try... catch](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch)
---
+++ app.js
@@ -0,0 +1,162 @@
+console.log("Hello World");
+
+// ==================== TOOLTIP RELATED CODE ====================
+// Tooltip elements to be updated when I hover over my upgrades
+const tooltip = document.getElementById("tooltip");
+const tooltipName = document.getElementById("tooltip-name");
+const tooltipCost = document.getElementById("tooltip-cost");
+const tooltipText = document.getElementById("tooltip-text");
+const tooltipError = document.getElementById("tooltip-error");
+
+// event listener on the whole body that tracks my mouse position. I can then use this pixel data to position the tooltip box absolutely!
+document.addEventListener("mousemove", function (e) {
+  let x = e.clientX;
+  let y = e.clientY;
+  tooltip.style.left = x + 15 + "px";
+  tooltip.style.top = y + "px";
+});
+
+function showTooltip(e) {
+  const index = e.target.getAttribute("data-upgrade");
+
+  tooltipName.textContent = myUpgrades[index].name;
+  tooltipCost.textContent = `Cost: ${myUpgrades[index].cost}`;
+  tooltipText.textContent = `Increases clicks per second by: ${myUpgrades[index].increase}`;
+  tooltip.style.display = "flex";
+}
+
+function hideTooltip() {
+  tooltip.style.display = "none";
+}
+
+const upgradesButton = document.getElementById("upgrades-toggle");
+
+let isDisplayed = false;
+
+upgradesButton.addEventListener("click", function () {
+  const upgradeContainer = document.getElementById("upgrades");
+
+  if (isDisplayed === true) {
+    isDisplayed = !isDisplayed;
+    upgradeContainer.style.display = "none";
+    upgradeContainer.style.visibility = "hidden";
+  } else if (isDisplayed === false) {
+    isDisplayed = !isDisplayed;
+    upgradeContainer.style.display = "flex";
+    upgradeContainer.style.visibility = "visible";
+  }
+});
+
+// ==================== INITIALISING CODE ====================
+// fetch the API
+async function getUpgradesFromAPI() {
+  const response = await fetch(
+    "https://cookie-upgrade-api.vercel.app/api/upgrades"
+  );
+  const data = await response.json();
+  return data;
+}
+
+// loop through the API and append to DOM
+async function initUpgrades() {
+  const container = document.getElementById("upgrades");
+  const upgrades = await getUpgradesFromAPI();
+
+  // takes the data from the API and pushes it into a local array so that when I want to reference the upgrades I'm not making fetch requests every time.
+  for (upgrade of upgrades) {
+    myUpgrades.push(upgrade);
+  }
+
+  // Creates the actual upgrade elements and appends them to the upgrades container in the DOM.
+  for (i = 0; i < myUpgrades.length; i++) {
+    const upgradeDiv = document.createElement("div");
+    upgradeDiv.classList.add("upgrade");
+    upgradeDiv.setAttribute("data-upgrade", i);
+
+    upgradeDiv.addEventListener("click", function (element) {
+      const index = element.target.getAttribute("data-upgrade");
+      if (stats.clicks >= myUpgrades[index].cost) {
+        stats.clicks -= myUpgrades[index].cost;
+        stats.clicksPerSecond += myUpgrades[index].increase;
+        updateInterface();
+      } else if (stats.clicks <= myUpgrades[index].cost) {
+        tooltipError.textContent =
+          "Not enough Clicks to buy this".toUpperCase();
+        tooltipError.style.display = "";
+        setTimeout(function () {
+          tooltipError.textContent = "";
+        }, 3000);
+      }
+    });
+
+    upgradeDiv.addEventListener("mouseover", showTooltip);
+    upgradeDiv.addEventListener("mouseleave", hideTooltip);
+
+    container.appendChild(upgradeDiv);
+  }
+}
+
+function updateInterface() {
+  clicksText.textContent = `     ${stats.clicks.toFixed(0)}`;
+  cpsText.textContent = ` ${stats.clicksPerSecond}`;
+}
+
+function initCookie() {
+  const cookie = document.getElementById("cookie-image");
+  cookie.addEventListener("click", function () {
+    stats.clicks++;
+  });
+}
+
+function startInterval() {
+  setInterval(function () {
+    stats.clicks += stats.clicksPerSecond / 10;
+    updateInterface();
+    saveGame();
+  }, 100);
+}
+
+function saveGame() {
+  localStorage.setItem("stats", JSON.stringify(stats));
+}
+
+function loadGame() {
+  const data = localStorage.getItem("stats");
+  const savedData = JSON.parse(data);
+
+  if (savedData) {
+    stats.clicks = savedData.clicks;
+    stats.clicksPerSecond = savedData.clicksPerSecond;
+  } else {
+    stats.clicks = 0;
+    stats.clicksPerSecond = 0;
+  }
+}
+
+// ========= GLOBAL VARIABLES =========
+const clicksText = document.getElementById("clicks");
+const cpsText = document.getElementById("clicks-per-second");
+
+let stats = {
+  clicks: 0,
+  clicksPerSecond: 0,
+};
+
+let myUpgrades = [];
+
+// INITIALISE THE GAME
+loadGame();
+initUpgrades();
+initCookie();
+startInterval();
+
+// FOR TESTING PURPOSES
+function resetGame() {
+  stats.clicks = 0;
+  stats.clicksPerSecond = 0;
+  localStorage.setItem("stats", JSON.stringify(stats));
+}
+
+function giveThousand() {
+  stats.clicks += 1000;
+}
